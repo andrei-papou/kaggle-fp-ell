f0f941b33b87611df18023a48dcf17fe4bf52e35
andrei-papou <andrei.v.papou@gmail.com>
2022-10-30 21:31:52 +0200

    Phraseology notebook, requirements caching, WandB logger.

--- containers/vscode/bin/build.sh
+++ containers/vscode/bin/build.sh
@@ -1,5 +1,6 @@
 docker build \
     --build-arg GITHUB_TOKEN="$GITHUB_TOKEN" \
+    --build-arg WANDB_TOKEN="$WANDB_TOKEN" \
     --build-arg CACHEBUST="$(date +%s)" \
     -t vscode-kaggle-fp-ell \
     -f containers/vscode/docker/Dockerfile \
